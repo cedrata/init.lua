039a03d66b275f9126483c0c1e0a949f19ea8158
cedrata <greggius9891@gmail.com>
2023-11-10 16:40:52 +0100

    [UPDATE] - neotest commands

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -13,7 +13,7 @@ require("neotest").setup({
     -- your neotest config here
     adapters = {
         require("neotest-go")({
-            args = {"-count=1", "-v"}
+            args = {"-count=1"}
         }),
     },
     icons = {
@@ -36,8 +36,5 @@ require("neotest").setup({
 })
 
 vim.keymap.set("n", "<leader>ts", function() require("neotest").summary.toggle() end)
-vim.keymap.set("n", "<leader>tr", function() require("neotest").run.run() end) -- run closest test to cursor
-vim.keymap.set("n", "<leader>ta", function() require("neotest").run.run({ suite = true }) end) -- run all tests
-vim.keymap.set("n", "<leader>tc", function() require("neotest").run.run({ suite = true, extra_args = {"-coverprofile='cover.out'"} }) end) -- run all tests and store coverage
-vim.keymap.set("n", "<leader>td", function() require("neotest").diagnostic() end)
-vim.keymap.set("n", "<leader>to", function() require("neotest").output_panel.toggle() end)
+vim.keymap.set("n", "<leader>ta", function() require("neotest").run.run({ suite = true }) end)
+
