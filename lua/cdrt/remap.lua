c9f9d031403403782c01ccfc11e202c495bc2939
cedrata <greggius9891@gmail.com>
2024-07-15 15:07:08 +0200

    feat: add diagnostic project wise

--- lua/cdrt/remap.lua
+++ lua/cdrt/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- paste without overiding buffer
-vim.keymap.set("x", "<leader>p", "\"_dP")
+-- vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+vim.keymap.set("n", "<leader>pd", vim.diagnostic.setqflist)
